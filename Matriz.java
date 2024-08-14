a116d50f8d0c48de73538143f033ae907bff07df
gabriel <89615327+gabrielentediado@users.noreply.github.com>
2024-08-14 16:32:57 -0300

    Add files via upload

---
+++ Matriz.java
@@ -0,0 +1,51 @@
+package br.aprendendo;
+import java.util.Scanner;
+//a classe consegue conter em si várias funções
+public class Matriz {
+    //esses são os atributos
+    private int linhas;
+    private int colunas;
+    private int [][] elementos;
+    //objeto para o scanner
+    private Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //a diferença é que no C, as structs não criam objetos, mas algo semelhante a registros
+        //armazenam em um tipo de dados, várias variáveis
+        //o new serve para instanciar, semelhante ao (struct Aluno aluno[];) ou (Struct Matriz matriz[][];)
+
+        Matriz minhamatriz = new Matriz(3,3);
+        minhamatriz.AdicionarElementos();
+        minhamatriz.mostrarMatriz();
+    }
+
+    //parece as structs do C
+
+    public Matriz(int linhas, int colunas) {
+        //o this serve para criar os objetos e apontar para os atributos
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.elementos = new int[linhas][colunas];
+    }
+
+    public void AdicionarElementos(){
+
+        for (int i = 0; i < linhas; i++){
+            for (int j = 0; j < colunas; j++){
+                System.out.println("Digite o número da linha:"+ i + "e coluna:" + j);
+                elementos[i][j]= scanner.nextInt();
+            };
+
+        };
+
+    }
+    public void mostrarMatriz(){
+        for (int i = 0; i < linhas; i++){
+            for(int j = 0; j < colunas; j++){
+                System.out.print(elementos[i][j]);
+            };
+        System.out.println();
+        };
+    }
+
+}
